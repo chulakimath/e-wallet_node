4971b8dadada57c36d9ce08859aa686c6cbb8395
chulakimath <santoshchulakimath222@gmail.com>
2025-07-17 23:10:57 +0530

    cron

--- src/server.js
+++ src/server.js
@@ -5,10 +5,12 @@ import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js"
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+if(process.env.NODE_ENV=="production")job.start()
 const PORT = process.env.PORT ?? 5001;
 app.use(ratelimiter);
 app.use(express.json());
@@ -16,8 +18,9 @@ app.use(express.json());
 // transaction /api/transactions
 app.use("/api/transactions",transactionsRoute)
 
-
-
+app.get("/api/health",(req,res)=>{
+    res.status(200).json({status:"ok"})
+})
 
 initDB().then(() => {
     app.listen(PORT, () => {
